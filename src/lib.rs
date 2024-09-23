ddd2a6585208c3252d48b01066b46abb756411d4
lhenkelm <lars.henkelmann@cern.ch>
2024-09-23 22:20:57 +0200

    tests: more involved example

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-pub fn ashay_igspay(english : &str) -> String {
+pub fn ashay_igspay_atinlay(english : &str) -> String {
     english.to_owned()
 }
 
@@ -6,15 +6,60 @@ pub fn ashay_igspay(english : &str) -> String {
 mod tests {
     use super::*;
 
+    // consonant examples
     #[test]
     fn test_first() {
-        let result = ashay_igspay("first");
+        let result = ashay_igspay_atinlay("first");
         assert_eq!(result, "irstfay");
+    }
+
+    #[test]
+    fn test_pigs() {
+        let result = ashay_igspay_atinlay("pigs");
+        assert_eq!(result, "igspay");
+    }
+
+    #[test]
+    fn test_latin() {
+        let result = ashay_igspay_atinlay("latin");
+        assert_eq!(result, "atinlay");
     }
     
     #[test]
+    fn test_banana() {
+        let result = ashay_igspay_atinlay("banana");
+        assert_eq!(result, "ananabay");
+    }
+
+    // vowel examples 
+    #[test]
     fn test_apple() {
-        let result = ashay_igspay("apple");
+        let result = ashay_igspay_atinlay("apple");
         assert_eq!(result, "applehay");
+    }
+    
+    #[test]
+    fn test_ear() {
+        let result = ashay_igspay_atinlay("ear");
+        assert_eq!(result, "earhay");
+    }
+    
+    #[test]
+    fn test_omelet() {
+        let result = ashay_igspay_atinlay("omelet");
+        assert_eq!(result, "omelethay");
+    }
+    
+    // sentence examples
+    #[test]
+    fn test_this_is_pigs_latin() {
+        let result = ashay_igspay_atinlay("This is pigs latin.");
+        assert_eq!(result, "Isthay ishay igspay atinlay.")
+    }
+    
+    #[test]
+    fn test_easy_innit() {
+        let result = ashay_igspay_atinlay("This is all quite easy, is it not?");
+        assert_eq!(result, "Isthay ishay allhay itequay easyhay, ishay ithay otnay?")
     }
 }
