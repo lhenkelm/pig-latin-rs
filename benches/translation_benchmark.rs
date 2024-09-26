8649c088e1d2cf5f3ab2b55158bcc9c23f484b12
lhenkelm <lars.henkelmann@cern.ch>
2024-09-26 23:21:24 +0200

    feat: tweak benchmarking

--- benches/translation_benchmark.rs
+++ benches/translation_benchmark.rs
@@ -6,9 +6,9 @@ use lipsum::lipsum_with_rng;
 use pig_latin::translate;
 
 fn lorem_ipsum(bencher: &mut Criterion){
-    let mut group = bencher.benchmark_group("lorem_ipsum");
-    for n_words in [10, 25, 50, 100, 250, 500, 1_000, 2_500, 5_000, 25_000, 50_000] {
-        for seed in [0u64, 1, 2, 42, 69, 123456789]{       
+    for seed in [0u64, 1, 2, 42, 69, 123456789]{       
+        let mut group = bencher.benchmark_group(format!("lorem_ipsum_seed{seed}"));
+        for n_words in [10, 25, 50, 100, 250, 500, 1_000, 2_500, 5_000, 25_000, 50_000] {
             let rng = Pcg64Mcg::seed_from_u64(seed);
             let test_input = lipsum_with_rng(rng, n_words);
             let n_bytes = test_input.len();
@@ -16,11 +16,11 @@ fn lorem_ipsum(bencher: &mut Criterion){
             group.bench_with_input(
                 BenchmarkId::from_parameter(format!("n_words:{n_words}-seed:{seed}")), 
                 &n_bytes,
-                |b, &_ | b.iter( || translate(black_box(&test_input)))
+                |b, &_ | b.iter_with_large_drop( || translate(black_box(&test_input)))
             );
         }
+        group.finish();
     }
-    group.finish();
 }
 
 criterion_group!(benches, lorem_ipsum);
