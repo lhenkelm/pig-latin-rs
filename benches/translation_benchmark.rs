5f77c3c6fef02640476128825f50c66a8f813f21
lhenkelm <lars.henkelmann@cern.ch>
2024-09-26 22:56:38 +0200

    test: an overall benchmark

---
+++ benches/translation_benchmark.rs
@@ -0,0 +1,28 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput, BenchmarkId};
+use rand_pcg::Pcg64Mcg;
+use rand_core::SeedableRng;
+use lipsum::lipsum_with_rng;
+
+use pig_latin::translate;
+
+fn lorem_ipsum(bencher: &mut Criterion){
+    let mut group = bencher.benchmark_group("lorem_ipsum");
+    for n_words in [10, 25, 50, 100, 250, 500, 1_000, 2_500, 5_000, 25_000, 50_000] {
+        for seed in [0u64, 1, 2, 42, 69, 123456789]{       
+            let rng = Pcg64Mcg::seed_from_u64(seed);
+            let test_input = lipsum_with_rng(rng, n_words);
+            let n_bytes = test_input.len();
+            group.throughput(Throughput::Bytes(n_bytes as u64));
+            group.bench_with_input(
+                BenchmarkId::from_parameter(format!("n_words:{n_words}-seed:{seed}")), 
+                &n_bytes,
+                |b, &_ | b.iter( || translate(black_box(&test_input)))
+            );
+        }
+    }
+    group.finish();
+}
+
+criterion_group!(benches, lorem_ipsum);
+criterion_main!(benches);
+
