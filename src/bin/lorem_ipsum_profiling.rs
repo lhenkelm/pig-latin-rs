bf1a7e717ee05b2bf197f79c72ae10243a26a0fc
lhenkelm <lars.henkelmann@cern.ch>
2024-09-28 12:13:51 +0200

    feat: tweak lorem ipsum profile

--- src/bin/lorem_ipsum_profiling.rs
+++ src/bin/lorem_ipsum_profiling.rs
@@ -9,11 +9,13 @@ use pig_latin::translate;
 fn main(){
     let mut total_in_bytes = 0; 
     let mut total_out_bytes = 0; 
-    for i in 0..100 {
+    for i in 0..10 {
         let rng = Pcg64Mcg::seed_from_u64(i);
         let test_input = lipsum_with_rng(rng, 1_000_000);
         total_in_bytes += test_input.len();
+        println!("passing {} bytes to translate ...", test_input.len());
         let result = translate(black_box(&test_input));
+        println!("... translated into {} bytes", result.len());
         total_out_bytes += result.len();
     }
     println!("translated {total_in_bytes} UTF8 bytes into {total_out_bytes} UTF8 bytes");
