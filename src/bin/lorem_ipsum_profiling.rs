2e0febce13088f49b98f48055833083c57ed7d47
lhenkelm <lars.henkelmann@cern.ch>
2024-09-28 11:39:01 +0200

    feat: working profilable exe

---
+++ src/bin/lorem_ipsum_profiling.rs
@@ -0,0 +1,20 @@
+use std::hint::black_box;
+
+use rand_pcg::Pcg64Mcg;
+use rand_core::SeedableRng;
+use lipsum::lipsum_with_rng;
+
+use pig_latin::translate;
+
+fn main(){
+    let mut total_in_bytes = 0; 
+    let mut total_out_bytes = 0; 
+    for i in 0..100 {
+        let rng = Pcg64Mcg::seed_from_u64(i);
+        let test_input = lipsum_with_rng(rng, 1_000_000);
+        total_in_bytes += test_input.len();
+        let result = translate(black_box(&test_input));
+        total_out_bytes += result.len();
+    }
+    println!("translated {total_in_bytes} UTF8 bytes into {total_out_bytes} UTF8 bytes");
+}
