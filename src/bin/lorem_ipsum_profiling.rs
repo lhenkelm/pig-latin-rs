1b57391d6fa300901722549edd4b8e0a62553534
lhenkelm <lars.henkelmann@cern.ch>
2024-09-28 14:12:19 +0200

    feat: longer-running profile

--- src/bin/lorem_ipsum_profiling.rs
+++ src/bin/lorem_ipsum_profiling.rs
@@ -9,7 +9,7 @@ use pig_latin::translate;
 fn main(){
     let mut total_in_bytes = 0; 
     let mut total_out_bytes = 0; 
-    for i in 0..10 {
+    for i in 0..40 {
         let rng = Pcg64Mcg::seed_from_u64(i);
         let test_input = lipsum_with_rng(rng, 1_000_000);
         total_in_bytes += test_input.len();
